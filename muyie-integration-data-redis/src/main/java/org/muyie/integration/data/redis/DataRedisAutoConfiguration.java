af3d3e13bcfe886795a24abe5ee40387bc828da1
qizhaohong <qizhaohong_sh@qq.com>
2020-03-23 18:07:47 +0800

    Initial commit

--- muyie-integration-data-redis/src/main/java/org/muyie/integration/data/redis/DataRedisAutoConfiguration.java
+++ muyie-integration-data-redis/src/main/java/org/muyie/integration/data/redis/DataRedisAutoConfiguration.java
@@ -1,8 +1,34 @@
 package org.muyie.integration.data.redis;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 
 @Configuration
 public class DataRedisAutoConfiguration {
+
+  @Bean
+  public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+    return new RedisLockRegistry(redisConnectionFactory, "muyie-r-lock");
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(
+      RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    FastJsonRedisSerializer<?> serializer = new FastJsonRedisSerializer<>(Object.class);
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(serializer);
+    redisTemplate.setHashKeySerializer(serializer);
+    redisTemplate.setHashValueSerializer(serializer);
+    redisTemplate.afterPropertiesSet();
+    return redisTemplate;
+  }
 
 }
