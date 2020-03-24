67a51346ee8ff4e1cfa90a4e6917543c42309af3
qizhaohong <qizhaohong_sh@qq.com>
2020-03-24 14:03:26 +0800

    Add docs

---
+++ muyie-integration-data-redis/README.md
@@ -0,0 +1,61 @@
+# muyie-integration-data-redis
+
+Redis 是一个开源（BSD许可）的，内存中的数据结构存储系统，它可以用作数据库、缓存和消息中间件。 它支持多种类型的数据结构，如 字符串（strings）， 散列（hashes）， 列表（lists）， 集合（sets）， 有序集合（sorted sets） 与范围查询， bitmaps， hyperloglogs 和 地理空间（geospatial） 索引半径查询。 Redis 内置了 复制（replication），LUA脚本（Lua scripting）， LRU驱动事件（LRU eviction），事务（transactions） 和不同级别的 磁盘持久化（persistence）， 并通过 Redis哨兵（Sentinel）和自动 分区（Cluster）提供高可用性（high availability）。
+
+- https://github.com/antirez/redis
+- http://www.redis.cn/
+
+本项目是基本 spring-boot-starter-data-redis 模块搭建的，一是开箱即用，避免重复造轮子；二是增强基础功能，支持Spring Cache 和分布式锁的集成。
+
+## 快速开始
+
+在你的Spring Boot项目中添加以下依赖：
+
+```
+<dependency>
+    <groupId>com.github.zhycn</groupId>
+    <artifactId>muyie-integration-data-redis</artifactId>
+    <version>${lastest.version}</version>
+</dependency>
+```
+
+添加Redis的属性配置参数：
+
+```
+# Redis数据库索引（默认为0）  
+spring.redis.database=0
+
+# Redis服务器地址（单机模式）
+spring.redis.host=192.168.10.1
+
+# Redis服务器连接端口（单机模式）
+spring.redis.port=6379
+
+# Redis集群环境地址（集群环境）
+# spring.redis.cluster.nodes=192.168.10.1:6379,192.168.10.2:6379,192.168.10.3:6379
+
+# Redis服务器连接密码（默认为空）
+spring.redis.password=
+
+# 连接池最大连接数（使用负值表示没有限制，默认为8）
+spring.redis.pool.max-active=200
+
+# 连接池最大阻塞等待时间（使用负值表示没有限制） 
+spring.redis.pool.max-wait=-1
+
+# 连接池中的最大空闲连接（默认为8）
+spring.redis.pool.max-idle=8
+
+# 连接池中的最小空闲连接（默认为0）
+spring.redis.pool.min-idle=0
+
+# 连接超时时间（毫秒）
+spring.redis.timeout=1000
+```
+
+## 使用向导
+
+- [RedisTempate](./docs/redis-tempate.md)
+- [Redisson](./docs/redisson.md)
+- [Redis分布式锁](./docs/redis-lock.md)
+- [Spring Cache](./docs/spring-cache.md)
--- muyie-integration-data-redis/src/main/java/org/muyie/integration/data/redis/DataRedisAutoConfiguration.java
+++ muyie-integration-data-redis/src/main/java/org/muyie/integration/data/redis/DataRedisAutoConfiguration.java
@@ -1,7 +1,16 @@
 package org.muyie.integration.data.redis;
 
+import java.util.Arrays;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,25 +19,70 @@ import org.springframework.integration.redis.util.RedisLockRegistry;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class DataRedisAutoConfiguration {
 
+  private final RedisConnectionFactory redisConnectionFactory;
+
+  public DataRedisAutoConfiguration(RedisConnectionFactory redisConnectionFactory) {
+    this.redisConnectionFactory = redisConnectionFactory;
+  }
+
+  /**
+   * 基于Spring Integration Redis实现的分布式锁
+   * 
+   * @param redisConnectionFactory 连接工厂对象
+   * @return RedisLockRegistry 锁对象
+   */
   @Bean
+  @ConditionalOnMissingBean
   public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
-    return new RedisLockRegistry(redisConnectionFactory, "muyie-r-lock");
+    return new RedisLockRegistry(redisConnectionFactory, "muyie-rlock");
   }
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate(
+  @ConditionalOnMissingBean(name = "redisTemplate")
+  public RedisTemplate<Object, Object> redisTemplate(
       RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    FastJsonRedisSerializer<?> serializer = new FastJsonRedisSerializer<>(Object.class);
+    RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory);
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(serializer);
-    redisTemplate.setHashKeySerializer(serializer);
-    redisTemplate.setHashValueSerializer(serializer);
+
+    // 自定义序列化配置
+    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+    FastJsonRedisSerializer<?> fastJsonRedisSerializer =
+        new FastJsonRedisSerializer<>(Object.class);
+    redisTemplate.setConnectionFactory(redisConnectionFactory);
+    redisTemplate.setKeySerializer(stringRedisSerializer);
+    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+    redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+    redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
     redisTemplate.afterPropertiesSet();
     return redisTemplate;
+  }
+
+  /**
+   * 自定义新的键名生成规则，默认的生成规则：{@link SimpleKeyGenerator}
+   * 
+   * @return 新的键名生成规则
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public KeyGenerator keyGenerator() {
+    return (target, method, params) -> {
+      StringBuilder sb = new StringBuilder();
+      sb.append(target.getClass().getName());
+      sb.append("::" + method.getName() + ":");
+      Arrays.asList(params).forEach(obj -> sb.append(obj.toString()));
+      return sb.toString();
+    };
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public CacheManager cacheManager() {
+    RedisCacheManager cacheManager =
+        RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory).build();
+    return cacheManager;
   }
 
 }
